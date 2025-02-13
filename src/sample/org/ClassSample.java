9b5d8dcf7228ce4747aecca8f1dd0a35c9472028
indu1803 <eindu1989@gmail.com>
2025-02-13 14:30:32 +0530

    push

--- src/sample/org/ClassSample.java
+++ src/sample/org/ClassSample.java
@@ -3,7 +3,7 @@ package sample.org;
 public class ClassSample {
 	public static void main(String[] args)
 	{
-System.out.println("am person 1");
+System.out.println("hai");
 System.out.println("iam person 3");
 System.out.println("iam person 2.1");
 System.out.println("after person 2");
