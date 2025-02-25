66cb3023385d6cbe988d396b2eaa3e893e5b5d5a
indu1803 <eindu1989@gmail.com>
2025-02-25 12:27:36 +0530

    push

--- src/sample/org/ClassSample.java
+++ src/sample/org/ClassSample.java
@@ -4,7 +4,7 @@ public class ClassSample {
 	public static void main(String[] args)
 	{
 System.out.println("hai");
-System.out.println("iam person 3 star");
+System.out.println("iam person 3 star *");
 System.out.println("iam person 2.1");
 System.out.println("after person 2");
 System.out.println("before person 3");
